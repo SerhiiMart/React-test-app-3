ee516e12d842369a8a50317a4ea21c5fbce6a490
Serhii <malgothsm@gmail.com>
2021-04-03 21:21:38 +0300

    Finished Hero section

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import HeroS from '../HeroS';
+// import Cards from '../Cards';
+// import Footer from '../Footer';
+
+function Home() {
+  return (
+    <>
+      <HeroS />
+      {/* <Cards />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default Home;
