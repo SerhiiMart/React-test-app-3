302a433797415235fb90908730d6fa9cbb39a59e
Serhii <malgothsm@gmail.com>
2021-04-05 13:46:50 +0300

    Working on Footer

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../../App.css';
 import HeroS from '../HeroS';
 import Cards from '../Cards';
-// import Footer from '../Footer';
+import Footer from '../Footer';
 
 function Home() {
   return (
     <>
       <HeroS />
       <Cards />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
